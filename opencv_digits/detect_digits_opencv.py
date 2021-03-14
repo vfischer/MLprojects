2b6573e43ebe589eca5f1e6baead6823ffb48776
Vinnie <vincent.fj.fischer@gmail.com>
2021-03-14 15:30:10 -0500

    update digit searcher with opencv

--- opencv_digits/detect_digits_opencv.py
+++ opencv_digits/detect_digits_opencv.py
@@ -89,75 +89,85 @@ warped_h2o = four_point_transform(gray_h2o, displayCnt_h2o.reshape(4, 2))
 output_h2o = four_point_transform(cropped_h2o, displayCnt_h2o.reshape(4, 2))
 
 # Images are resized to get more pixels (better threshold later)
-warped_env= imutils.resize(warped_env, height=500)
-warped_h2o= imutils.resize(warped_h2o, height=500)
+warped_env= imutils.resize(warped_env, height=200)
+warped_h2o= imutils.resize(warped_h2o, height=200)
 
-cv2.imshow("warped_env", warped_env)
-cv2.imshow("warped_h2o", warped_h2o)
+
 # At this point, the two images are well found
 # Now let's find the digits
 
+warped_env_humi = warped_env[41:100, 37:110]
+warped_env_temp = warped_env[134:174, 60:103]
+warped_h2o_resis = warped_h2o[53:116, 91:213]
+
+cv2.imshow("warped_env_humi", warped_env_humi)
+cv2.imshow("warped_env_temp", warped_env_temp)
+cv2.imshow("warped_h2o_resis", warped_h2o_resis)
+
 # threshold the warped image, then apply a series of morphological
 # operations to cleanup the thresholded image
-thresh_env = cv2.adaptiveThreshold(warped_env,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-    cv2.THRESH_BINARY_INV,131,0)
-#thresh_env = cv2.threshold(warped_env, 210, 255,
-#	cv2.THRESH_BINARY)[1]
-kernel_env = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
-thresh_env = cv2.morphologyEx(thresh_env, cv2.MORPH_OPEN, kernel_env)
+thresh_env_humi = cv2.adaptiveThreshold(warped_env_humi,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+    cv2.THRESH_BINARY_INV,31,1)
+#thresh_env_humi = cv2.threshold(warped_env_humi, 230, 255,
+#	cv2.THRESH_BINARY_INV)[1]
+kernel_env_humi = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+thresh_env_humi = cv2.morphologyEx(thresh_env_humi, cv2.MORPH_OPEN, kernel_env_humi)
 
-thresh_h2o = cv2.adaptiveThreshold(warped_h2o,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-    cv2.THRESH_BINARY_INV,131,0)
-#thresh_h2o = cv2.threshold(warped_h2o, 68, 255,
-#	cv2.THRESH_BINARY)[1]
+thresh_env_temp = cv2.adaptiveThreshold(warped_env_temp,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+    cv2.THRESH_BINARY_INV,41,1)
+#thresh_env_temp = cv2.threshold(warped_env_temp, 182, 255,
+#	cv2.THRESH_BINARY_INV)[1]
+kernel_env_temp = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+thresh_env_temp = cv2.morphologyEx(thresh_env_temp, cv2.MORPH_OPEN, kernel_env_temp)
 
-kernel_h2o = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
+thresh_h2o_resis = cv2.adaptiveThreshold(warped_h2o_resis,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+    cv2.THRESH_BINARY_INV,31,1)
+#thresh_h2o_resis = cv2.threshold(warped_h2o_resis, 62, 255,
+#	cv2.THRESH_BINARY_INV)[1]
+kernel_h2o_resis = cv2.getStructuringElement(cv2.MORPH_RECT, (5,5))
 #thresh_h2o = cv2.erode(thresh_h2o,kernel_h2o,iterations = 1)
-thresh_h2o = cv2.morphologyEx(thresh_h2o, cv2.MORPH_OPEN, kernel_h2o)
+thresh_h2o_resis = cv2.morphologyEx(thresh_h2o_resis, cv2.MORPH_OPEN, kernel_h2o_resis)
 
-thresh_env= imutils.resize(thresh_env, height=200)
-thresh_h2o= imutils.resize(thresh_h2o, height=200)
-
-thresh_env_humi = thresh_env[41:100, 37:110]
-thresh_env_temp = thresh_env[134:174, 60:103]
-thresh_h2o_resis = thresh_h2o[53:116, 91:213]
 
 cv2.imshow("thresh_env_humi", thresh_env_humi)
 cv2.imshow("thresh_env_temp", thresh_env_temp)
 cv2.imshow("thresh_h2o_resis", thresh_h2o_resis)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 
 # find contours in the thresholded image, then initialize the
 # digit contours lists
-cnts_env = cv2.findContours(thresh_h2o_resis.copy(), cv2.RETR_EXTERNAL,
+cnts_env_humi = cv2.findContours(thresh_env_humi.copy(), cv2.RETR_EXTERNAL,
 	cv2.CHAIN_APPROX_SIMPLE)
-cnts_env = imutils.grab_contours(cnts_env)
-digitCnts_env = []
+cnts_env_humi = imutils.grab_contours(cnts_env_humi)
+digitCnts_env_humi = []
 # loop over the digit area candidates
-for c in cnts_env:
+for c in cnts_env_humi:
 	# compute the bounding box of the contour
 	(x, y, w, h) = cv2.boundingRect(c)
 	print("w:",w,"h:",h)
 	# if the contour is sufficiently large, it must be a digit
-	if w >= 20 and (h >= 30 and h <= 70):
-		digitCnts_env.append(c)
+	if (w >= 10 and w <=45) and (h >= 30 and h <= 70):
+		digitCnts_env_humi.append(c)
 
 # sort the contours from left-to-right, then initialize the
 # actual digits themselves
-digitCnts_env = contours.sort_contours(digitCnts_env, method="left-to-right")[0]
-digits_env = []
+digitCnts_env_humi = contours.sort_contours(digitCnts_env_humi, method="left-to-right")[0]
+digits_env_humi = []
 
 # loop over each of the digits
-for c in digitCnts_env:
+for c in digitCnts_env_humi:
 	# extract the digit ROI
 	(x, y, w, h) = cv2.boundingRect(c)
-	roi = thresh_h2o_resis[y:y + h, x:x + w]
+	roi = thresh_env_humi[y:y + h, x:x + w]
 	# compute the width and height of each of the 7 segments
 	# we are going to examine
 	(roiH, roiW) = roi.shape
 	(dW, dH) = (int(roiW * 0.25), int(roiH * 0.15))
 	dHC = int(roiH * 0.05)
 	# define the set of 7 segments
-	segments_env = [
+	segments_env_humi = [
 		((0, 0), (w, dH)),	# top
 		((0, 0), (dW, h // 2)),	# top-left
 		((w - dW, 0), (w, h // 2)),	# top-right
@@ -166,9 +176,9 @@ for c in digitCnts_env:
 		((w - dW, h // 2), (w, h)),	# bottom-right
 		((0, h - dH), (w, h))	# bottom
 	]
-	on = [0] * len(segments_env)
+	on = [0] * len(segments_env_humi)
 	# loop over the segments
-	for (i, ((xA, yA), (xB, yB))) in enumerate(segments_env):
+	for (i, ((xA, yA), (xB, yB))) in enumerate(segments_env_humi):
 		# extract the segment ROI, count the total number of
 		# thresholded pixels in the segment, and then compute
 		# the area of the segment
@@ -182,10 +192,134 @@ for c in digitCnts_env:
 			# lookup the digit and draw it on the image
 		if tuple(on) in DIGITS_LOOKUP:
 			digit = DIGITS_LOOKUP[tuple(on)]
-			digits_env.append(digit)
+			digits_env_humi.append(digit)
 	#cv2.rectangle(thresh_env_humi, (x, y), (x + w, y + h), (0, 255, 0), 1)
 	#cv2.putText(thresh_env_humi, str(digit), (x - 2, y - 2), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
-print(digits_env)
+print(digits_env_humi)
+
+######################################################
+# find contours in the thresholded image, then initialize the
+# digit contours lists
+cnts_env_temp = cv2.findContours(thresh_h2o_resis.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts_env_temp = imutils.grab_contours(cnts_env_temp)
+digitCnts_env_temp = []
+# loop over the digit area candidates
+for c in cnts_env_temp:
+	# compute the bounding box of the contour
+	(x, y, w, h) = cv2.boundingRect(c)
+	print("w:",w,"h:",h)
+	# if the contour is sufficiently large, it must be a digit
+	if (w >= 15 and w <=35) and (h >= 30 and h <= 70):
+		digitCnts_env_temp.append(c)
+
+# sort the contours from left-to-right, then initialize the
+# actual digits themselves
+digitCnts_env_temp = contours.sort_contours(digitCnts_env_temp, method="left-to-right")[0]
+digits_env_temp = []
+
+# loop over each of the digits
+for c in digitCnts_env_temp:
+	# extract the digit ROI
+	(x, y, w, h) = cv2.boundingRect(c)
+	roi = thresh_h2o_resis[y:y + h, x:x + w]
+	# compute the width and height of each of the 7 segments
+	# we are going to examine
+	(roiH, roiW) = roi.shape
+	(dW, dH) = (int(roiW * 0.25), int(roiH * 0.15))
+	dHC = int(roiH * 0.05)
+	# define the set of 7 segments
+	segments_env_temp = [
+		((0, 0), (w, dH)),	# top
+		((0, 0), (dW, h // 2)),	# top-left
+		((w - dW, 0), (w, h // 2)),	# top-right
+		((0, (h // 2) - dHC) , (w, (h // 2) + dHC)), # center
+		((0, h // 2), (dW, h)),	# bottom-left
+		((w - dW, h // 2), (w, h)),	# bottom-right
+		((0, h - dH), (w, h))	# bottom
+	]
+	on = [0] * len(segments_env_temp)
+	# loop over the segments
+	for (i, ((xA, yA), (xB, yB))) in enumerate(segments_env_temp):
+		# extract the segment ROI, count the total number of
+		# thresholded pixels in the segment, and then compute
+		# the area of the segment
+		segROI = roi[yA:yB, xA:xB]
+		total = cv2.countNonZero(segROI)
+		area = (xB - xA) * (yB - yA)
+		# if the total number of non-zero pixels is greater than
+		# 50% of the area, mark the segment as "on"
+		if total / float(area) > 0.5:
+			on[i]= 1
+			# lookup the digit and draw it on the image
+		if tuple(on) in DIGITS_LOOKUP:
+			digit = DIGITS_LOOKUP[tuple(on)]
+			digits_env_temp.append(digit)
+	#cv2.rectangle(thresh_env_temp_humi, (x, y), (x + w, y + h), (0, 255, 0), 1)
+	#cv2.putText(thresh_env_temp_humi, str(digit), (x - 2, y - 2), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
+print(digits_env_temp)
+
+#######################################################################
+# find contours in the thresholded image, then initialize the
+# digit contours lists
+cnts_h2o_temp = cv2.findContours(thresh_h2o_resis.copy(), cv2.RETR_EXTERNAL,
+	cv2.CHAIN_APPROX_SIMPLE)
+cnts_h2o_temp = imutils.grab_contours(cnts_h2o_temp)
+digitCnts_h2o_temp = []
+# loop over the digit area candidates
+for c in cnts_h2o_temp:
+	# compute the bounding box of the contour
+	(x, y, w, h) = cv2.boundingRect(c)
+	print("w:",w,"h:",h)
+	# if the contour is sufficiently large, it must be a digit
+	if (w >= 20 and w <=50) and (h >= 30 and h <= 70):
+		digitCnts_h2o_temp.append(c)
+
+# sort the contours from left-to-right, then initialize the
+# actual digits themselves
+digitCnts_h2o_temp = contours.sort_contours(digitCnts_h2o_temp, method="left-to-right")[0]
+digits_h2o_temp = []
+
+# loop over each of the digits
+for c in digitCnts_h2o_temp:
+	# extract the digit ROI
+	(x, y, w, h) = cv2.boundingRect(c)
+	roi = thresh_h2o_resis[y:y + h, x:x + w]
+	# compute the width and height of each of the 7 segments
+	# we are going to examine
+	(roiH, roiW) = roi.shape
+	(dW, dH) = (int(roiW * 0.25), int(roiH * 0.15))
+	dHC = int(roiH * 0.05)
+	# define the set of 7 segments
+	segments_h2o_temp = [
+		((0, 0), (w, dH)),	# top
+		((0, 0), (dW, h // 2)),	# top-left
+		((w - dW, 0), (w, h // 2)),	# top-right
+		((0, (h // 2) - dHC) , (w, (h // 2) + dHC)), # center
+		((0, h // 2), (dW, h)),	# bottom-left
+		((w - dW, h // 2), (w, h)),	# bottom-right
+		((0, h - dH), (w, h))	# bottom
+	]
+	on = [0] * len(segments_h2o_temp)
+	# loop over the segments
+	for (i, ((xA, yA), (xB, yB))) in enumerate(segments_h2o_temp):
+		# extract the segment ROI, count the total number of
+		# thresholded pixels in the segment, and then compute
+		# the area of the segment
+		segROI = roi[yA:yB, xA:xB]
+		total = cv2.countNonZero(segROI)
+		area = (xB - xA) * (yB - yA)
+		# if the total number of non-zero pixels is greater than
+		# 50% of the area, mark the segment as "on"
+		if total / float(area) > 0.5:
+			on[i]= 1
+			# lookup the digit and draw it on the image
+		if tuple(on) in DIGITS_LOOKUP:
+			digit = DIGITS_LOOKUP[tuple(on)]
+			digits_h2o_temp.append(digit)
+	#cv2.rectangle(thresh_h2o_temp_humi, (x, y), (x + w, y + h), (0, 255, 0), 1)
+	#cv2.putText(thresh_h2o_temp_humi, str(digit), (x - 2, y - 2), cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 255, 0), 2)
+print(digits_h2o_temp)
 
 # display the digits
 #print(u"{}{}.{} \u00b0C".format(*digits_env))
